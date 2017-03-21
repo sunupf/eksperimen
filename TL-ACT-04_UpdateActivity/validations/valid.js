1235d8ad73b407fe7b1f775df54aef37e8160c04
Sunu Pinasthika Fajar <sunupf@gmail.com>
2017-03-22 06:53:49 +0700

    update activity eksperimen

--- TL-ACT-04_UpdateActivity/validations/valid.js
+++ TL-ACT-04_UpdateActivity/validations/valid.js
@@ -20,59 +20,16 @@ var valid = function(param){
      
     //text queries 
     //query masih fail
-    var data = client.querySync("SELECT todo_id from activities where todo_id in (SELECT id FROM todos where user_id in (SELECT id from users where email='"+user+"'))")
+    var data = client.querySync("SELECT id from activities where todo_id in (SELECT id FROM todos where user_id in (SELECT id from users where email='"+user+"'))")
     // var indexValidTodoId = _.findIndex(data, function(datum) { return datum.todo_id == value; });
     var i = Math.floor(Math.random() * (data.length) );
-    todoId = parseInt(data[i].todo_id)
+    id = parseInt(data[i].id)
 
-    console.log(todoId);
-
-    var indexNotBelongs = _.findIndex(param.validations, function(validation) { return validation == "^belongs_parent_id"; });
-    if(indexNotBelongs >= 0){
-      var notBelongs = client.querySync("SELECT id FROM todos where user_id not in (SELECT id from users where email='"+user+"')")
-      var j = Math.floor(Math.random() * (notBelongs.length) );
-      todoId = parseInt(notBelongs[j].id)
-    }
-
-    var indexNotNumeric = _.findIndex(param.validations, function(validation) { return validation == "^numeric_parent_id"; });
-    if(indexNotNumeric>=0){
-      var character = ['a','b','c','d','e']
-      var k = Math.floor(Math.random() * (character.length) );
-      todoId = character[k]
-    }
-
-    var indexNotBelongs = _.findIndex(param.validations, function(validation) { return validation == "^required_parent_id"; });
-    if(indexNotBelongs >= 0){
-      todoId = ""
-    }
-
-    var dataActivities = {}
-    var id = 0;
-    var indexActNotBelongsToParentId = _.findIndex(param.validations, function(validation) { return validation == "^belongs_id_to_parent_id"; });
     var indexActNotBelongs = _.findIndex(param.validations, function(validation) { return validation == "^belongs_id"; });
-
-    if(typeof todoId === "number"){
-      dataActivities = client.querySync("SELECT id from activities where todo_id = "+todoId)
-      if(dataActivities.length > 0){
-        var l = Math.floor(Math.random() * (dataActivities.length) );
-        id = parseInt(dataActivities[l].id);
-      }
-
-      if(indexActNotBelongsToParentId >= 0){
-        var notBelongsToParentId = client.querySync("SELECT id FROM activities where todo_id != "+todoId)
-        if(notBelongsToParentId.length > 0){
-          var m = Math.floor(Math.random() * (notBelongsToParentId.length) );
-          id = parseInt(notBelongsToParentId[m].id)
-        }
-      }
-
-      if(indexActNotBelongs >= 0){
-        var notBelongsId = client.querySync("SELECT id FROM activities where todo_id not in (SELECT id FROM todos where user_id in (SELECT id from users where email='"+user+"'))")
-        if(notBelongsId.length > 0){
-          var n = Math.floor(Math.random() * (notBelongsId.length) );
-          id = parseInt(notBelongsId[n].id)
-        }
-      }
+    if(indexActNotBelongs>=0){
+      var data = client.querySync("SELECT id from activities where todo_id not in (SELECT id FROM todos where user_id in (SELECT id from users where email='"+user+"'))")
+      var i = Math.floor(Math.random() * (data.length) );
+      id = parseInt(data[i].id)
     }
 
     var indexActNotNumeric = _.findIndex(param.validations, function(validation) { return validation == "^numeric_id"; });
@@ -88,7 +45,7 @@ var valid = function(param){
     }
 
     client.end();
-    return "http://todo.app/activity/"+id+"#"+todoId;
+    return "http://todo.app/activity/"+id;
   }
 
   return callback;
